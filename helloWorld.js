58655cf6b44439d87461bb2e84b9eb3c6239c96c
Serge Thomas <smughemuzi@gmail.com>
2024-02-20 10:19:23 +0200

    added user input and returned to window

--- helloWorld.js
+++ helloWorld.js
@@ -3,3 +3,7 @@ console.log("Git is awesome!");
 
 // Print to window
 alert("Git is awesome!");
+
+// added user input and returned answer to prompt on window
+let userInput = prompt("What is the meaning of life ?")
+alert(userInput);
